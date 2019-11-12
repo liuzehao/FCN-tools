0ca859daa618f20bcb3be0d6181fea3a06d55f4f
刘泽豪 <liuzehao139@163.com>
2019-11-12 14:49:50 +0800

    Update json_to_dataset.py
    Labelme标注工具 json批量生成，解决了两个问题： 1、生成的label.png不是灰度图8bits, 2、生成的label.png中，标注物体label不具备统一性

--- json_to_dataset.py
+++ json_to_dataset.py
@@ -3,86 +3,83 @@ import json
 import os
 import os.path as osp
 import warnings
- 
+import copy
+
+import numpy as np
 import PIL.Image
+from skimage import io
 import yaml
- 
+
 from labelme import utils
-import base64
- 
+
+NAME_LABEL_MAP = {
+    '_background_': 0,
+    "cat": 1,
+    "dog": 2,
+    # "basketball_court": 3,
+    # "ground_track_field": 4,
+}
+
+LABEL_NAME_MAP = ['0: _background_',
+                  '1: cat',
+                  '2: dog']
+
+
 def main():
-    warnings.warn("This script is aimed to demonstrate how to convert the\n"
-                  "JSON file to a single image dataset, and not to handle\n"
-                  "multiple JSON files to generate a real-use dataset.")
     parser = argparse.ArgumentParser()
     parser.add_argument('json_file')
     parser.add_argument('-o', '--out', default=None)
     args = parser.parse_args()
- 
+
     json_file = args.json_file
-    if args.out is None:
-        out_dir = osp.basename(json_file).replace('.', '_')
-        out_dir = osp.join(osp.dirname(json_file), out_dir)
-    else:
-        out_dir = args.out
-    if not osp.exists(out_dir):
-        os.mkdir(out_dir)
- 
-    count = os.listdir(json_file) 
-    for i in range(0, len(count)):
-        path = os.path.join(json_file, count[i])
+
+    list = os.listdir(json_file)
+    for i in range(0, len(list)):
+        path = os.path.join(json_file, list[i])
+        filename = list[i][:-5]       # .json
         if os.path.isfile(path):
             data = json.load(open(path))
-            
-            if data['imageData']:
-                imageData = data['imageData']
-            else:
-                imagePath = os.path.join(os.path.dirname(path), data['imagePath'])
-                with open(imagePath, 'rb') as f:
-                    imageData = f.read()
-                    imageData = base64.b64encode(imageData).decode('utf-8')
-            img = utils.img_b64_to_arr(imageData)
-            label_name_to_value = {'_background_': 0}
-            for shape in data['shapes']:
-                label_name = shape['label']
-                if label_name in label_name_to_value:
-                    label_value = label_name_to_value[label_name]
-                else:
-                    label_value = len(label_name_to_value)
-                    label_name_to_value[label_name] = label_value
-            
-            # label_values must be dense
-            label_values, label_names = [], []
-            for ln, lv in sorted(label_name_to_value.items(), key=lambda x: x[1]):
-                label_values.append(lv)
-                label_names.append(ln)
-            assert label_values == list(range(len(label_values)))
-            
-            lbl = utils.shapes_to_label(img.shape, data['shapes'], label_name_to_value)
-            
-            captions = ['{}: {}'.format(lv, ln)
-                for ln, lv in label_name_to_value.items()]
-            lbl_viz = utils.draw_label(lbl, img, captions)
-            
-            out_dir = osp.basename(count[i]).replace('.', '_')
-            out_dir = osp.join(osp.dirname(count[i]), out_dir)
+            img = utils.image.img_b64_to_arr(data['imageData'])
+            lbl, lbl_names = utils.shape.labelme_shapes_to_label(img.shape, data['shapes'])  # labelme_shapes_to_label
+
+            # modify labels according to NAME_LABEL_MAP
+            lbl_tmp = copy.copy(lbl)
+            for key_name in lbl_names:
+                old_lbl_val = lbl_names[key_name]
+                new_lbl_val = NAME_LABEL_MAP[key_name]
+                lbl_tmp[lbl == old_lbl_val] = new_lbl_val
+            lbl_names_tmp = {}
+            for key_name in lbl_names:
+                lbl_names_tmp[key_name] = NAME_LABEL_MAP[key_name]
+
+            # Assign the new label to lbl and lbl_names dict
+            lbl = np.array(lbl_tmp, dtype=np.int8)
+            lbl_names = lbl_names_tmp
+            print ('lbl_names: ',lbl_names)
+            # captions = ['%d: %s' % (l, name) for l, name in enumerate(lbl_names)]
+            # captions = ['0: _background_', '1: cat', '2: dog']
+
+            lbl_viz = utils.draw.draw_label(lbl, img, LABEL_NAME_MAP)
+            out_dir = osp.basename(list[i]).replace('.', '_')
+            out_dir = osp.join(osp.dirname(list[i]), out_dir)
             if not osp.exists(out_dir):
                 os.mkdir(out_dir)
- 
-            PIL.Image.fromarray(img).save(osp.join(out_dir, 'img.png'))
-            #PIL.Image.fromarray(lbl).save(osp.join(out_dir, 'label.png'))
-            utils.lblsave(osp.join(out_dir, 'label.png'), lbl)
-            PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, 'label_viz.png'))
- 
+
+            PIL.Image.fromarray(img).save(osp.join(out_dir, '{}.png'.format(filename)))
+            PIL.Image.fromarray(lbl).save(osp.join(out_dir, '{}_gt.png'.format(filename)))
+            PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, '{}_viz.png'.format(filename)))
+
             with open(osp.join(out_dir, 'label_names.txt'), 'w') as f:
-                for lbl_name in label_names:
+                for lbl_name in lbl_names:
                     f.write(lbl_name + '\n')
- 
+
             warnings.warn('info.yaml is being replaced by label_names.txt')
-            info = dict(label_names=label_names)
+            info = dict(label_names=lbl_names)
             with open(osp.join(out_dir, 'info.yaml'), 'w') as f:
                 yaml.safe_dump(info, f, default_flow_style=False)
- 
+
             print('Saved to: %s' % out_dir)
+
+
 if __name__ == '__main__':
     main()
