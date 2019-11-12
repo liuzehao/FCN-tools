3513240ec9d51b368aa12f84716b328e4cd60f77
刘泽豪 <liuzehao139@163.com>
2019-11-12 14:50:46 +0800

    Update move.py
    修改了剪切为复制

--- move.py
+++ move.py
@@ -9,12 +9,12 @@ for i,filename in enumerate(filelists):
 	print(filename)
 	filename = os.path.join(filedir, filename)
 
-	shutil.move(os.path.join(filename,'label.png'),os.path.join(outdir_a,'training'))
+	shutil.copy(os.path.join(filename,'label.png'),os.path.join(outdir_a,'training'))
 	name1 = 'train-' + str(i+1).zfill(3) + '.png'
 
 	os.rename(os.path.join(outdir_a,'training/label.png'),os.path.join(outdir_a,'training',name1))
 
 
-	shutil.move(os.path.join(filename,'img.png'),os.path.join(outdir_i,'training'))
+	shutil.copy(os.path.join(filename,'img.png'),os.path.join(outdir_i,'training'))
 	name2 = 'train-' + str(i+1).zfill(3) + '.png'
 	os.rename(os.path.join(outdir_i,'training/img.png'),os.path.join(outdir_i,'training',name2))
